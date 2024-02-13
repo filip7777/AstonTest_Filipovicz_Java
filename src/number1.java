296463d9859ee0139428588b58ae2305c2c300c2
Filipovicz <filipovicz1986@gmail.com>
2024-02-13 20:28:59 +0300

    first commit

---
+++ src/number1.java
@@ -0,0 +1,17 @@
+public class number1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 8;
+        if (a > b) {
+            System.out.println("a>b");
+        } else if (a < b) {
+            System.out.println("a<b");
+        } else if (a == b) {
+            System.out.println("a=b");
+        }
+        System.out.println(a+b+" сложение");
+        System.out.println(a-b+" вычитание");
+        System.out.println(1.0*a*b+ " умножение");
+        System.out.println(1.0*a/b+ " деление");
+    }
+}
---
+++ src/number2.java
@@ -0,0 +1,15 @@
+public class number2 {
+
+    public static void main(String[] args) {
+
+        String a = "Привет";
+        String b = new String("Привет!");
+
+        if(a.equals(b)){
+            System.out.println("Строки индентичны");
+        }
+        else{
+            System.out.println("Строки не идентичны");
+        }
+    }
+}
